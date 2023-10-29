99a5529c52fef66f6fbe90c53c93e522402bbde9
RudyCortedano <rudycortedano028@gmail.com>
2023-10-29 15:02:49 -0600

    2.añadiendo handlebars

--- src/app.js
+++ src/app.js
@@ -3,21 +3,27 @@ const helmet = require('helmet');
 const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./utils/errorHandler');
+const handlebars = require('express-handlebars');
 require('dotenv').config();
 
 // Esta es nuestra aplicación
 const app = express();
-
 // Middlewares 
 app.use(express.json());
 app.use(helmet({
     crossOriginResourcePolicy: false,
 }));
 app.use(cors());
-
+app.set('view engine', 'handlebars');
+app.set('views', __dirname+'/views');
+app.engine('handlebars', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'index'
+}));
 app.use(router);
 app.get('/', (req, res) => {
-    return res.send("Welcome to express!");
+    // return res.send("Welcome to express!");
+    return res.render("home");
 })
 
 // middlewares después de las rutas
